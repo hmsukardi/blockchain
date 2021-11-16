8fa5c51d8b1c3bbd34777eb900bb13f915ec18be
hmsukardi <hamid.sukardi@binus.ac.id>
2021-11-17 01:02:54 +0700

    look last block

--- blockchain.py
+++ blockchain.py
@@ -66,3 +66,9 @@ class Blockchain(object):
             'sender': sender
         })
         return self.last_block['index'] + 1
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+        
